18b499c743034208fab16459275d8ce61f22f840
Alex Onceanu <4lex.onceanu@gmail.com>
2026-03-16 16:31:48 +0100

    Special label class with boing animation

---
+++ game/power_up/animated_char.gd
@@ -0,0 +1,16 @@
+extends Control
+
+#to change the duration or strength of the animation, change the curves
+@export var rotation_anim : Curve
+@export var scale_anim : Curve
+
+func spring(t : float) -> void:
+	$Movable.rotation_degrees = rotation_anim.sample(t)
+	$Movable.scale = scale_anim.sample(t) * Vector2(1.0, 1.0)
+
+func _on_anim_delay_timeout() -> void:
+	create_tween().tween_method(spring, 0.0, rotation_anim.max_domain, rotation_anim.max_domain)
+
+#starts the "boing" animation after "lag" seconds
+func activate(lag : float) -> void:
+	$AnimDelay.start(lag)
