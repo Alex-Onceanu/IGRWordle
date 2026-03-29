af1df8efe74347a5129f92a445c9814b5807925c
Francisco Henriques <franciscolealhenriques@usp.br>
2026-03-29 14:42:13 +0200

    Add first implementation of the LevelManager class
    This class deals with all data manipulation and logic regarding a single
    level, including:
    - resolving the current guess
    - updating the total score
    - checking whether a guess is valid
    - determining in which cell the next letter will be placed
    and so on.

--- game/level/level_manager.gd
+++ game/level/level_manager.gd
@@ -9,3 +9,62 @@ class_name LevelManager
 # - it keeps track of how many points the player has
 # - it keeps track of the "challenges" the player has to complete to gain coins (coin power-ups)
 # - it keeps track of word resolution (ordering of effect resolution function calls, total number of points scored, etc.)
+# TODO create files for each word size, or organize words.txt into a JSON with word sizes 
+const WORDS_PATH : String = "res://words.txt"
+
+@export var receiver: SignalBusReceiver
+
+var next_cell_to_fill_idx: int = 0
+var current_row: int = 0
+var current_guess: String = ""
+var grid: Grid
+
+
+func get_next_cell_to_fill() -> void:
+	return grid.get_cell_by_index(next_cell_to_fill_idx)
+
+
+func _get_next_row() -> int:
+	var idx = grid.resource.cell_layout.keys().find_custom(func(x): return x.x > current_row)
+	if idx == -1:
+		return current_row
+	return grid.resource.cell_layout.keys()[idx].x
+
+
+func _is_last_cell_in_row() -> bool:
+	return next_cell_to_fill_idx >= grid.get_cell_count() \
+		or grid.resource.cell_layout.keys()[next_cell_to_fill_idx].x > current_row
+
+
+func _is_valid_guess() -> bool:
+	return true
+	
+
+func _on_keyboard_character_pressed(c: String) -> void:
+	if _is_last_cell_in_row():
+		return
+	current_guess += c 
+	var letter_box := grid.get_cell_by_index(next_cell_to_fill_idx)
+	next_cell_to_fill_idx += 1
+	letter_box.letter = c
+	letter_box.status = LetterBox.Status.FULL
+
+
+func _on_keyboard_delete_pressed() -> void:
+	print("current guess is ", current_guess)
+	if current_guess == "":
+		return
+	current_guess = current_guess.erase(current_guess.length() - 1, 1)
+	next_cell_to_fill_idx -= 1
+	var letter_box := grid.get_cell_by_index(next_cell_to_fill_idx)
+	letter_box.letter = " "
+	letter_box.status = LetterBox.Status.EMPTY
+	
+
+func _on_keyboard_enter_pressed() -> void:
+	if not _is_valid_guess():
+		print("not a valid guess")
+		return
+	# TODO resolve guess, effects, and earn points
+	current_row = _get_next_row()
+	current_guess = ""
