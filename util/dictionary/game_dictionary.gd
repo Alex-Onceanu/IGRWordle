ec86f0dc84776635b10e361d5a3ec5b739bc6a96
Francisco Henriques <franciscolealhenriques@usp.br>
2026-03-29 19:42:54 +0200

    Add GameDictionary autoload
    This class is responsible for consulting the dictionary and providing an
    API to find information in it. It picks random words, check their
    existence in the dictionary, gets their definition, etc.

---
+++ util/dictionary/game_dictionary.gd
@@ -0,0 +1,38 @@
+extends Node
+## Class for loading a dictionary into the game and consulting it. It provides an API for
+## consulting whether a word is or not in the dictionary, and also to randomly pick words.
+
+const DICTIONARY_FILE_PATH = "res://util/dictionary/english_dictionary.json"
+var words: Dictionary = {}
+
+
+func _ready() -> void:
+	_load_dictionary(DICTIONARY_FILE_PATH)
+
+
+func pick_random_word_of_size(size: int) -> String:
+	return words[str(size)].keys().pick_random()
+
+
+func is_in_dictionary(word: String) -> bool:
+	var word_size = word.length()
+	return words[str(word_size)].has(word)
+
+
+func get_definition(word: String) -> Array:
+	if not is_in_dictionary(word):
+		return []
+	var word_size = word.length()
+	return words[str(word_size)][word]["MEANINGS"]
+
+
+func _load_dictionary(path: String) -> void:
+	if not FileAccess.file_exists(path):
+		print("Dictionary file does not exist.")
+		return
+	var data_file = FileAccess.open(path, FileAccess.READ)
+	var parsed_result = JSON.parse_string(data_file.get_as_text())
+	if not parsed_result is Dictionary:
+		print("Error loading dictionary.")
+		return 
+	words = parsed_result
