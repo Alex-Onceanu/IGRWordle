f61cfa42968998f3dad1bebf17ad4594b4bac14e
Francisco Henriques <franciscolealhenriques@usp.br>
2026-04-02 16:43:54 +0200

    Fix repeated letter guess validation bug
    There was a problem with the guess validation scheme, as it used a
    dictionary with the secret word to verify the correctness of each
    letter.
    
    The problem was that dictionaries only store one key/value pair, so
    words with repeated letters stored only the column of one of them. To
    fix that, `secret_word` was changed to be a dictionary that contains a
    letter as key, and an array of columns as value.
    
    Another bug this commit solves is the one of winning the game by
    guessing a word that has the secret word as its substring. To fix that,
    the win condition verification was changed to compare the number of
    correct letters to the size of the current guess (all the letters from
    the current guess are correct) instead of comparing it to the size of
    the secret word (n letters correct in this guess where n is the number
    of letters in the secret word).

--- game/level/level_manager.gd
+++ game/level/level_manager.gd
@@ -18,7 +18,7 @@ var current_guess: String = ""
 var correct_letters_in_guess: int = 0
 var grid: Grid
 var keyboard: Keyboard
-var secret_word: Dictionary[String, int] # (character, column)
+var secret_word: Dictionary[String, Array] # (character, list of column indices this character occurs)
 
 
 func choose_secret_word() -> void: 
@@ -31,7 +31,7 @@ func choose_secret_word() -> void:
 	print("cells in last row are: ", last_row_cells)
 	var secret_word_string := GameDictionary.pick_random_word_of_size(last_row_cells.size())
 	for i in secret_word_string.length():
-		secret_word[secret_word_string[i]] = last_row_cells.keys()[i].y
+		secret_word.get_or_add(secret_word_string[i], []).append(last_row_cells.keys()[i].y)
 	print("secret word is: ", secret_word_string)
 	print(secret_word)
 	
@@ -45,14 +45,14 @@ func resolve_guess() -> void:
 	for coord in row_letters:
 		_resolve_letter_guess(coord, row_letters[coord])
 		_resolve_letter_effect(coord, row_letters[coord])
-	if correct_letters_in_guess == secret_word.keys().size():
+	if correct_letters_in_guess == current_guess.length():
 		_win()
 	correct_letters_in_guess = 0
 
 
 func _resolve_letter_guess(coordinates: Vector2i, letter: LetterBox):
 	if letter.letter in secret_word.keys():
-		if coordinates.y == secret_word[letter.letter]:
+		if coordinates.y in secret_word[letter.letter]:
 			letter.correctness = LetterBox.Correctness.CORRECT
 			correct_letters_in_guess += 1
 		else:
