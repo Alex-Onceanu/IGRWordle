92f5c60a4596f2076cdf1004f113230ff00bb142
Francisco Henriques <franciscolealhenriques@usp.br>
2026-04-03 23:54:21 +0200

    Add RunManager as autoload
    Since this class orchestrates scene changes, it needs to be on the tree
    at all times.

--- game/run/run_manager.gd
+++ game/run/run_manager.gd
@@ -1,5 +1,4 @@
 extends Node
-class_name RunManager
 
 # this is the class that will deal with the flow of a run. It is responsible for establishing each step of the
 # main gameplay loop, getting data from it and transitioning between scenes according to it. It will:
@@ -10,11 +9,31 @@ class_name RunManager
 # That means that for all the buttons the player presses to go from one scene to another, the one class that deals with those
 # signals is this one
 @export var run_progression_list: Array[RunProgression]
+@export var level_generator: LevelGenerator
+
+var current_difficulty: RunProgression
+var current_level := 1
+var point_threshold := 0
 # NOTE: this script needs to:
 # - go to win screen when level is won
 # - go to game over screen when level is failed
 # - go to shop when button is pressed on win screen
 # - generate a new level when exiting shop
+
+
+func _create_next_level() -> Node:
+	if current_difficulty == null:
+		current_difficulty = run_progression_list[0]
+	level_generator.min_word_size = current_difficulty.min_word_size
+	level_generator.max_word_size = current_difficulty.max_word_size
+	level_generator.attempts = current_difficulty.attempts
+	point_threshold += current_difficulty.point_threshold_increase
+	level_generator.point_threshold = point_threshold
+	current_level += 1
+	for difficulty in run_progression_list:
+		if current_level >= difficulty.level_start:
+			current_difficulty = difficulty
+	return level_generator.generate_level()
 
 
 func _on_level_manager_level_lost() -> void:
@@ -26,8 +45,12 @@ func _on_level_manager_level_won() -> void:
 
 
 func _on_shop_manager_next_level_pressed() -> void:
-	pass # Replace with function body.
+	SceneSwitcher._change_to_scene(_create_next_level())
 
 
 func _on_winning_menu_next_level_pressed() -> void:
-	pass # Replace with function body.
+	SceneSwitcher.go_to_shop()
+
+
+func _on_main_menu_play_pressed() -> void:
+	SceneSwitcher._change_to_scene(_create_next_level())
