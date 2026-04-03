a9470589609abbb74ccb0148da828316272bef03
Francisco Henriques <franciscolealhenriques@usp.br>
2026-04-03 15:41:19 +0200

    Add base RunManager class
    With this base class, only the signal connections were done

--- game/run/run_manager.gd
+++ game/run/run_manager.gd
@@ -9,3 +9,25 @@ class_name RunManager
 # - go to a new level after the player leaves the shop
 # That means that for all the buttons the player presses to go from one scene to another, the one class that deals with those
 # signals is this one
+@export var run_progression_list: Array[RunProgression]
+# NOTE: this script needs to:
+# - go to win screen when level is won
+# - go to game over screen when level is failed
+# - go to shop when button is pressed on win screen
+# - generate a new level when exiting shop
+
+
+func _on_level_manager_level_lost() -> void:
+	pass
+
+
+func _on_level_manager_level_won() -> void:
+	pass # Replace with function body.
+
+
+func _on_shop_manager_next_level_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_winning_menu_next_level_pressed() -> void:
+	pass # Replace with function body.
