9f76e7c227c330aabd1695e12addfcf49c878484
msenart <mathieu.senart@gmail.com>
2026-04-07 23:51:43 +0200

    integrated definitions !

--- util/dictionary/game_dictionary.gd
+++ util/dictionary/game_dictionary.gd
@@ -19,12 +19,32 @@ func is_in_dictionary(word: String) -> bool:
 	return words[str(word_size)].has(word)
 
 
-func get_definition(word: String) -> Array:
+func get_definition(word: String) -> Dictionary:
 	if not is_in_dictionary(word):
-		return []
+		return {}
 	var word_size = word.length()
-	return words[str(word_size)][word]["MEANINGS"]
+	return words[str(word_size)][word]
 
+static func format_dictionary_data(data: Dictionary) -> String:
+	var bbcode = ""
+	if data.has("MEANINGS"):
+		bbcode += "[p][font_size=24][b]Definitions[/b][/font_size][/p]\n"
+		
+		for meaning in data["MEANINGS"]:
+			var part_of_speech = meaning[0]
+			var definition = meaning[1] + "."
+			
+			bbcode += "[color=blue][i]" + part_of_speech + "[/i][/color]: "
+			bbcode += definition + "\n"
+			bbcode += "\n"
+	if data.has("SYNONYMS") and data["SYNONYMS"].size() > 0:
+		bbcode += "[b][color=dark_green]Synonyms:[/color][/b] "
+		bbcode += ", ".join(data["SYNONYMS"]) + "\n\n"
+	
+	if data.has("ANTONYMS") and data["ANTONYMS"].size() > 0:
+		bbcode += "[b][color=red]Antonyms:[/color][/b] "
+		bbcode += ", ".join(data["ANTONYMS"]) + "\n"
+	return bbcode
 
 func _load_dictionary(path: String) -> void:
 	if not FileAccess.file_exists(path):
